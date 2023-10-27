7428ec41f8651760cab5f50c4c11a092ab778b29
Felicia Folatimilehin Owabumuwa Null <felowa4129@ctag.com>
2023-10-27 12:46:15 +0200

    using hostlistener to make directives dynamic

--- src/app/better-highlight.directive.ts
+++ src/app/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -9,5 +9,13 @@ export class BetterHighlightDirective implements OnInit {
 
   ngOnInit(): void {
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green')
+  }
+
+  @HostListener ('mouseenter') mouseEnt(){
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'purple')
+  }
+
+  @HostListener ('mouseleave') mouseOff(){
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'white')
   }
 }
