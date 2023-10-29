ae778368e2239c3d3256a8e7116756e3ee36d1f1
Felicia Folatimilehin Owabumuwa Null <felowa4129@ctag.com>
2023-10-29 15:48:03 +0100

    default project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,15 @@
 import { Component } from '@angular/core';
-import { UsersServiceService } from './users-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [UsersServiceService],
 })
 export class AppComponent {
-  newUser: { name: string, status: boolean } = {
-    name: '',
-    status: false
-  };
-
-
-  activeUsers: { name: string, status: boolean }[] = [];
-  inactiveUsers: { name: string, status: boolean }[] = [];
   
-  constructor(private userService: UsersServiceService){}
+  constructor(){}
   ngOnInit(): void {
-    this.newUser = this.userService.newUser
-    this.activeUsers = this.userService.activeUsers
-    this.inactiveUsers = this.userService.inactiveUsers
+    
   }
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,18 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ActiveUsersComponent } from './active-users/active-users.component';
-import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
-import { UsersServiceService } from './users-service.service';
-import { CounterServiceService } from './counter-service.service';
-import { CreateUsersComponent } from './create-users/create-users.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    ActiveUsersComponent,
-    InactiveUsersComponent,
-    CreateUsersComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    
   ],
-  providers: [UsersServiceService, CounterServiceService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
